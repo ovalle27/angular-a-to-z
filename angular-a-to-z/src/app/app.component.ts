665864894ff2e8a0286daccc26f24c1310395d7d
Adrian Ovalle <ovalle27@gmail.com>
2024-11-09 13:33:09 -0600

    Chages up to the minute 41:40 at the video. This covers Structural Directive *ngIf and how to use it with 'then' and 'else' conditioners.

--- angular-a-to-z/src/app/app.component.ts
+++ angular-a-to-z/src/app/app.component.ts
@@ -1,20 +1,25 @@
 import { Component, IterableDiffers } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  items = [
+  // items = [
+  //   new WishItem('To Learn Angular'),
+  //   new WishItem('Have some coffee', true),
+  //   new WishItem('Find grass that cuts itself')
+  // ];
+  items : WishItem[] = [
     new WishItem('To Learn Angular'),
-    new WishItem('Have some coffee', true),
-    new WishItem('Find grass that cuts itself')
+    // new WishItem('Have some coffee', true),
+    // new WishItem('Find grass that cuts itself')
   ];
 
   title = 'Whish List';
