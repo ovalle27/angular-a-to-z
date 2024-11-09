8e348dfb10c8563cd4efd1773049c33ef9f30ff2
Adrian Ovalle <ovalle27@gmail.com>
2024-11-09 02:41:02 -0600

    Min 28.44 at the video

--- angular-a-to-z/src/app/app.component.ts
+++ angular-a-to-z/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-a-to-x';
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Have some coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
+
+  title = 'Whish List';
 }
